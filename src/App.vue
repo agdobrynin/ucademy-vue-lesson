eafa409b45f03162ad0f292aabcb7a14099ade2a
Alexey Dobrynin <ag.dobrynin@gmail.com>
2020-01-14 22:48:40 +0400

    Start component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template lang="pug">
-  div#app
-    h1
+  div.container.pt-3#app
+    h1.alert.alert-primary Hello World!
+    div.form-group
+        label Email
+        input(
+            name="email"
+            type="email"
+            :value="email").form-control
 </template>
 
 <script>
-export default {
-  name: "app",
-}
+    export default {
+        name: "app",
+        data(){
+            return {
+                email: "email@yahoo.com",
+            }
+        },
+    }
 </script>
 
 <style lang="scss">
-
+    @import "~bootstrap/scss/bootstrap";
 </style>
