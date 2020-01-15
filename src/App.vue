584d5f7641da6818c0e60a7ec5fafd4fd76cf6bb
Alexey Dobrynin <ag.dobrynin@gmail.com>
2020-01-15 14:58:57 +0400

    Confirm password field, and button for submit with disabled attribute

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,44 @@
     form
         div.form-group
             label Email
-            input(name="email" type="email" v-model="email" :class="{'is-invalid':$v.email.$invalid}"
+            input(name="email" type="email" v-model="email"
+                :class="{'is-invalid':$v.email.$invalid, 'is-valid':!$v.email.$invalid}"
                 @input="$v.email.$touch()").form-control
             div(class="invalid-feedback" v-if="!$v.email.email") Please input correct email address
             div(class="invalid-feedback" v-if="!$v.email.required") Email is mandatory field
 
         div.form-group
             label Password
-            input(name="password" type="password" v-model="password" :class="{'is-invalid':$v.password.$invalid}"
+            input(name="password" type="password" v-model="password"
+                :class="{'is-invalid':$v.password.$invalid, 'is-valid':!$v.password.$invalid}"
             @input="$v.password.$touch()").form-control
             div(class="invalid-feedback" v-if="!$v.password.required && !$v.password.$dirty") Password is mandatory field
-            div(class="invalid-feedback" v-if="password.length < 6 && $v.password.$dirty")
+            div(class="invalid-feedback" v-if="$v.password.$invalid && $v.password.$dirty")
                 span
-                    | Password must be more then {{ $v.password.$params.minLength.min }} symbols.
+                    | Password must be more then #[strong {{ $v.password.$params.minLength.min }}] symbols.
                     | Now password is #[strong {{ password.length }}] symbols.
+
+        div.form-group
+            label Confirm password
+            input(name="confirmPassword" type="password" v-model="confirmPassword"
+                :class="{'is-invalid':$v.confirmPassword.$invalid, 'is-valid':!$v.confirmPassword.$invalid}"
+            @input="$v.confirmPassword.$touch()").form-control
+            div(class="invalid-feedback" v-if="!$v.confirmPassword.required && !$v.confirmPassword.$dirty") Confirm password
+            div(class="invalid-feedback" v-if="$v.confirmPassword.$invalid && $v.confirmPassword.$dirty") Password not matched
 
         div.form-check.form-group
             input(v-model="isDebug" type="checkbox" id="checkDebug")
             label(for="checkDebug") &nbsp;Debug validation info
+
+        button(class="btn btn-primary" type="submit" :disabled="!canSubmit") Submit
 
     pre(v-if="isDebug") Debug validation: {{ $v }}
 </template>
 
 <script>
 
-    import { required, email, minLength } from "vuelidate/lib/validators";
+    import { required, email, minLength, sameAs } from "vuelidate/lib/validators";
+    const MIN_PASSWORD_LENGTH = 6;
 
     export default {
         name: "app",
@@ -37,6 +50,13 @@
                 email: '',
                 isDebug: false,
                 password: '',
+                confirmPassword: '',
+            }
+        },
+        computed: {
+            canSubmit() {
+                const validator = this.$v;
+                return !validator.email.$invalid && !validator.password.$invalid&& !validator.confirmPassword.$invalid;
             }
         },
         validations: {
@@ -45,9 +65,15 @@
                 email: email,
             },
             password: {
-                minLength: minLength(6),
+                minLength: minLength(MIN_PASSWORD_LENGTH),
                 required: required,
             },
+            confirmPassword: {
+                required: required,
+                confirmPassword: sameAs( (vue) => {
+                    return vue.password;
+                }),
+            }
         }
     }
 </script>
