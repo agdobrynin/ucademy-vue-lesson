a04491fb9c86864bc70c5ef5aa72c3aeee08b1c9
Alexey Dobrynin <ag.dobrynin@gmail.com>
2020-01-15 13:29:47 +0400

    Show error form with debug

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template lang="pug">
   div.container.pt-3#app
     h1.alert.alert-primary Hello World!
-    div.form-group
-        label Email
-        input(
-            name="email"
-            type="email"
-            v-model="email"
-            @input="$v.email.$touch()").form-control
-    pre {{ $v }}
+    form
+        div.form-group
+            label Email
+            input(
+                name="email"
+                type="email"
+                v-model="email"
+                :class="{'is-invalid':$v.email.$invalid}"
+                @input="$v.email.$touch()").form-control
+            div(
+                class="invalid-feedback"
+                v-if="!$v.email.email") Please input correct email address
+            div(
+                class="invalid-feedback"
+                v-if="!$v.email.required") Email is mandatory field
+
+        div.form-check.form-group
+            input(
+                v-model="isDebug"
+                type="checkbox"
+                id="checkDebug")
+            label(
+                for="checkDebug"
+            ) &nbsp;Debug validation info
+
+    pre(v-if="isDebug") Debug validation: {{ $v }}
 </template>
 
 <script>
-    
+
     import { required, email } from "vuelidate/lib/validators";
 
     export default {
@@ -20,6 +38,7 @@
         data(){
             return {
                 email: '',
+                isDebug: false,
             }
         },
         validations: {
